99cd96f759d0ed908a30f69a27f21ac00b3fcfaa
Mario Gonzalez <mariogonzalez@gmail.com>
2013-09-18 22:39:14 -0400

    Cleaned up some anti-patterns in Demo.js. Made mouse functions inherritable. Pausing request animation from on focus lost.

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,8 @@
 // This is the utility code to drive the chipmunk demos. The demos are rendered using
 // a single canvas on the page.
+window.COLLISIONGROUP_NONE = (1 << 0);
+window.COLLISIONGROUP_BALL = (1 << 2);
+window.COLLISIONGROUP_FLOOR = (1 << 3);
 
 var v = cp.v;
 
@@ -9,83 +12,26 @@ var GRABABLE_MASK_BIT = 1<<31;
 var NOT_GRABABLE_MASK = ~GRABABLE_MASK_BIT;
 
 var Demo = function() {
-	var space = this.space = new cp.Space();
+	this.space = new cp.Space();
 	this.remainder = 0;
 	this.fps = 0;
 	this.mouse = v(0,0);
 	this.simulationTime = 0;
 	this.drawTime = 0;
 
-	var self = this;
-	var canvas2point = this.canvas2point = function(x, y) {
-		return v(x / self.scale, 480 - y / self.scale);
-	};
-
-	this.point2canvas = function(point) {
-			return v(point.x * self.scale, (480 - point.y) * self.scale);
-	};
-
 	// HACK HACK HACK - its awful having this here, and its going to break when we
 	// have multiple demos open at the same time.
-	this.canvas.onmousemove = function(e) {
-		self.mouse = canvas2point(e.clientX, e.clientY);
-	};
-
-	/*
-	this.canvas.onmousedown = function(e) {
-		radius = 10;
-		mass = 3;
-		body = space.addBody(new cp.Body(mass, cp.momentForCircle(mass, 0, radius, v(0, 0))));
-		body.setPos(canvas2point(e.clientX, e.clientY));
-		circle = space.addShape(new cp.CircleShape(body, radius, v(0, 0)));
-		circle.setElasticity(0.5);
-		return circle.setFriction(1);
-	};*/
-
-	var mouseBody = this.mouseBody = new cp.Body(Infinity, Infinity);
+  // Fixed?
+  this.canvas.onmouseup = this.onMouseUp.bind(this);
+  this.canvas.onmousemove = this.onMouseMove.bind(this);
+  this.canvas.onmousedown = this.onMouseDown.bind(this);
+  this.mouseBody = new cp.Body(Infinity, Infinity);
 
 	this.canvas.oncontextmenu = function(e) { return false; }
 
-	this.canvas.onmousedown = function(e) {
-		e.preventDefault();
-		var rightclick = e.which === 3; // or e.button === 2;
-		self.mouse = canvas2point(e.clientX, e.clientY);
-
-		if(!rightclick && !self.mouseJoint) {
-			var point = canvas2point(e.clientX, e.clientY);
-		
-			var shape = space.pointQueryFirst(point, GRABABLE_MASK_BIT, cp.NO_GROUP);
-			if(shape){
-				var body = shape.body;
-				var mouseJoint = self.mouseJoint = new cp.PivotJoint(mouseBody, body, v(0,0), body.world2Local(point));
-
-				mouseJoint.maxForce = 50000;
-				mouseJoint.errorBias = Math.pow(1 - 0.15, 60);
-				space.addConstraint(mouseJoint);
-			}
-		}
-
-		if(rightclick) {
-			self.rightClick = true;
-		}
-	};
-
-	this.canvas.onmouseup = function(e) {
-		var rightclick = e.which === 3; // or e.button === 2;
-		self.mouse = canvas2point(e.clientX, e.clientY);
-
-		if(!rightclick) {
-			if(self.mouseJoint) {
-				space.removeConstraint(self.mouseJoint);
-				self.mouseJoint = null;
-			}
-		}
-
-		if(rightclick) {
-			self.rightClick = false;
-		}
-	};
-
+  // Listen for lost focus and pause the demo
+  window.addEventListener("focus", this.run.bind(this), false);
+  window.addEventListener("blur", this.stop.bind(this), false);
 };
 
 var canvas = Demo.prototype.canvas = document.getElementsByTagName('canvas')[0];
@@ -97,12 +43,12 @@ var ctx = Demo.prototype.ctx = canvas.getContext('2d');
 // in floating point maths anyway.
 
 window.onresize = function(e) {
-	var width = Demo.prototype.width = canvas.width = window.innerWidth;
-	var height = Demo.prototype.height = canvas.height = window.innerHeight;
-	if (width/height > 640/480) {
-		Demo.prototype.scale = height / 480;
+	var width = Demo.prototype.width = canvas.width = 800; window.innerWidth;
+	var height = Demo.prototype.height = canvas.height = 500;//window.innerHeight;
+	if (width/height > 800/500) {
+		Demo.prototype.scale = height / 500;
 	} else {
-		Demo.prototype.scale = width / 640;
+		Demo.prototype.scale = width / 800;
 	}
 
 	Demo.resized = true;
@@ -123,6 +69,57 @@ Demo.prototype.update = function(dt) {
 	this.space.step(dt);
 };
 
+Demo.prototype.onMouseDown = function(e) {
+  e.preventDefault();
+  var rightclick = e.which === 3; // or e.button === 2;
+  this.mouse = this.canvas2point( e.clientX, e.clientY );
+
+  if ( !rightclick && !this.mouseJoint ) {
+    var point = this.canvas2point( e.clientX, e.clientY );
+
+    var shape = this.space.pointQueryFirst( point, GRABABLE_MASK_BIT, cp.NO_GROUP );
+    if ( shape ) {
+      var body = shape.body;
+      var mouseJoint = this.mouseJoint = new cp.PivotJoint( this.mouseBody, body, v( 0, 0 ), body.world2Local( point ) );
+
+      mouseJoint.maxForce = 50000;
+      mouseJoint.errorBias = Math.pow( 1 - 0.15, 60 );
+      this.space.addConstraint( mouseJoint );
+    }
+  }
+
+  if ( rightclick ) {
+    this.rightClick = true;
+  }
+};
+
+Demo.prototype.onMouseMove = function(e) {
+  this.mouse = this.canvas2point(e.clientX, e.clientY);
+};
+
+Demo.prototype.onMouseUp= function(e) {
+  var rightclick = e.which === 3; // or e.button === 2;
+  this.mouse = this.canvas2point( e.clientX, e.clientY );
+
+  if ( !rightclick ) {
+    if ( this.mouseJoint ) {
+      this.space.removeConstraint( this.mouseJoint );
+      this.mouseJoint = null;
+    }
+  }
+  if ( rightclick ) {
+    this.rightClick = false;
+  }
+};
+
+Demo.prototype.canvas2point = function(x,y) {
+  return v(x / this.scale, this.height - y / this.scale);
+};
+
+Demo.prototype.point2canvas = function(point) {
+  return v(point.x * this.scale, (this.height - point.y) * this.scale);
+};
+
 Demo.prototype.drawInfo = function() {
 	var space = this.space;
 
@@ -137,20 +134,20 @@ Demo.prototype.drawInfo = function() {
 		fpsStr = '--';
 	}
 	this.ctx.fillText("FPS: " + fpsStr, 10, 50, maxWidth);
-	this.ctx.fillText("Step: " + space.stamp, 10, 80, maxWidth);
+	this.ctx.fillText("Step: " + space.stamp, 10, 70, maxWidth);
 
 	var arbiters = space.arbiters.length;
 	this.maxArbiters = this.maxArbiters ? Math.max(this.maxArbiters, arbiters) : arbiters;
-	this.ctx.fillText("Arbiters: " + arbiters + " (Max: " + this.maxArbiters + ")", 10, 110, maxWidth);
+	this.ctx.fillText("Arbiters: " + arbiters + " (Max: " + this.maxArbiters + ")", 10, 90, maxWidth);
 
 	var contacts = 0;
 	for(var i = 0; i < arbiters; i++) {
 		contacts += space.arbiters[i].contacts.length;
 	}
 	this.maxContacts = this.maxContacts ? Math.max(this.maxContacts, contacts) : contacts;
-	this.ctx.fillText("Contact points: " + contacts + " (Max: " + this.maxContacts + ")", 10, 140, maxWidth);
-	this.ctx.fillText("Simulation time: " + this.simulationTime + " ms", 10, 170, maxWidth);
-	this.ctx.fillText("Draw time: " + this.drawTime + " ms", 10, 200, maxWidth);
+	this.ctx.fillText("Contact points: " + contacts + " (Max: " + this.maxContacts + ")", 10, 110, maxWidth);
+	this.ctx.fillText("Simulation time: " + this.simulationTime + " ms", 10, 130, maxWidth);
+	this.ctx.fillText("Draw time: " + this.drawTime + " ms", 10, 150, maxWidth);
 
 	if (this.message) {
 		this.ctx.fillText(this.message, 10, this.height - 50, maxWidth);
@@ -171,7 +168,7 @@ Demo.prototype.draw = function() {
 
 	this.space.eachShape(function(shape) {
 		ctx.fillStyle = shape.style();
-		shape.draw(ctx, self.scale, self.point2canvas);
+		shape.draw(ctx, self.scale, self);
 	});
 
 	// Draw collisions
@@ -207,7 +204,7 @@ Demo.prototype.draw = function() {
 
 	this.space.eachConstraint(function(c) {
 		if(c.draw) {
-			c.draw(ctx, self.scale, self.point2canvas);
+			c.draw(ctx, self.scale, self);
 		}
 	});
 
@@ -283,20 +280,23 @@ Demo.prototype.step = function(dt) {
 
 Demo.prototype.addFloor = function() {
 	var space = this.space;
-	var floor = space.addShape(new cp.SegmentShape(space.staticBody, v(0, 0), v(640, 0), 0));
+	var floor = space.addShape(new cp.SegmentShape(space.staticBody, v(0, 0), v(this.width, 0), 0));
 	floor.setElasticity(1);
 	floor.setFriction(1);
+  floor.group = window.COLLISIONGROUP_FLOOR;
 	floor.setLayers(NOT_GRABABLE_MASK);
 };
 
 Demo.prototype.addWalls = function() {
 	var space = this.space;
-	var wall1 = space.addShape(new cp.SegmentShape(space.staticBody, v(0, 0), v(0, 480), 0));
+	var wall1 = space.addShape(new cp.SegmentShape(space.staticBody, v(0, 0), v(0, this.height), 0));
+  wall1.group = window.COLLISIONGROUP_FLOOR;
 	wall1.setElasticity(1);
 	wall1.setFriction(1);
 	wall1.setLayers(NOT_GRABABLE_MASK);
 
-	var wall2 = space.addShape(new cp.SegmentShape(space.staticBody, v(640, 0), v(640, 480), 0));
+	var wall2 = space.addShape(new cp.SegmentShape(space.staticBody, v(this.width, 0), v(this.width, this.height), 0));
+  wall2.group = window.COLLISIONGROUP_FLOOR;
 	wall2.setElasticity(1);
 	wall2.setFriction(1);
 	wall2.setLayers(NOT_GRABABLE_MASK);
@@ -304,16 +304,16 @@ Demo.prototype.addWalls = function() {
 
 // Drawing helper methods
 
-var drawCircle = function(ctx, scale, point2canvas, c, radius) {
-	var c = point2canvas(c);
+var drawCircle = function(ctx, scale, demo, c, radius) {
+	var c = demo.point2canvas(c);
 	ctx.beginPath();
 	ctx.arc(c.x, c.y, scale * radius, 0, 2*Math.PI, false);
 	ctx.fill();
 	ctx.stroke();
 };
 
-var drawLine = function(ctx, point2canvas, a, b) {
-	a = point2canvas(a); b = point2canvas(b);
+var drawLine = function(ctx, demo, a, b) {
+	a = demo.point2canvas(a); b = demo.point2canvas(b);
 
 	ctx.beginPath();
 	ctx.moveTo(a.x, a.y);
@@ -321,9 +321,9 @@ var drawLine = function(ctx, point2canvas, a, b) {
 	ctx.stroke();
 };
 
-var drawRect = function(ctx, point2canvas, pos, size) {
-	var pos_ = point2canvas(pos);
-	var size_ = cp.v.sub(point2canvas(cp.v.add(pos, size)), pos_);
+var drawRect = function(ctx, demo, pos, size) {
+	var pos_ = demo.point2canvas(pos);
+	var size_ = cp.v.sub(demo.point2canvas(cp.v.add(pos, size)), pos_);
 	ctx.fillRect(pos_.x, pos_.y, size_.x, size_.y);
 };
 
@@ -345,8 +345,8 @@ var springPoints = [
 	v(1.00, 0.0)
 ];
 
-var drawSpring = function(ctx, scale, point2canvas, a, b) {
-	a = point2canvas(a); b = point2canvas(b);
+var drawSpring = function(ctx, scale, demo, a, b) {
+	a = demo.point2canvas(a); b = demo.point2canvas(b);
 	
 	ctx.beginPath();
 	ctx.moveTo(a.x, a.y);
@@ -370,86 +370,86 @@ var drawSpring = function(ctx, scale, point2canvas, a, b) {
 
 // **** Draw methods for Shapes
 
-cp.PolyShape.prototype.draw = function(ctx, scale, point2canvas)
+cp.PolyShape.prototype.draw = function(ctx, scale, demo)
 {
 	ctx.beginPath();
 
 	var verts = this.tVerts;
 	var len = verts.length;
-	var lastPoint = point2canvas(new cp.Vect(verts[len - 2], verts[len - 1]));
+	var lastPoint = demo.point2canvas(new cp.Vect(verts[len - 2], verts[len - 1]));
 	ctx.moveTo(lastPoint.x, lastPoint.y);
 
 	for(var i = 0; i < len; i+=2){
-		var p = point2canvas(new cp.Vect(verts[i], verts[i+1]));
+		var p = demo.point2canvas(new cp.Vect(verts[i], verts[i+1]));
 		ctx.lineTo(p.x, p.y);
 	}
 	ctx.fill();
 	ctx.stroke();
 };
 
-cp.SegmentShape.prototype.draw = function(ctx, scale, point2canvas) {
+cp.SegmentShape.prototype.draw = function(ctx, scale, demo) {
 	var oldLineWidth = ctx.lineWidth;
 	ctx.lineWidth = Math.max(1, this.r * scale * 2);
-	drawLine(ctx, point2canvas, this.ta, this.tb);
+	drawLine(ctx, demo, this.ta, this.tb);
 	ctx.lineWidth = oldLineWidth;
 };
 
-cp.CircleShape.prototype.draw = function(ctx, scale, point2canvas) {
-	drawCircle(ctx, scale, point2canvas, this.tc, this.r);
+cp.CircleShape.prototype.draw = function(ctx, scale, demo) {
+	drawCircle(ctx, scale, demo, this.tc, this.r);
 
 	// And draw a little radian so you can see the circle roll.
-	drawLine(ctx, point2canvas, this.tc, cp.v.mult(this.body.rot, this.r).add(this.tc));
+	drawLine(ctx, demo, this.tc, cp.v.mult(this.body.rot, this.r).add(this.tc));
 };
 
 
 // Draw methods for constraints
 
-cp.PinJoint.prototype.draw = function(ctx, scale, point2canvas) {
+cp.PinJoint.prototype.draw = function(ctx, scale, demo) {
 	var a = this.a.local2World(this.anchr1);
 	var b = this.b.local2World(this.anchr2);
 	
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = "grey";
-	drawLine(ctx, point2canvas, a, b);
+	drawLine(ctx, demo, a, b);
 };
 
-cp.SlideJoint.prototype.draw = function(ctx, scale, point2canvas) {
+cp.SlideJoint.prototype.draw = function(ctx, scale, demo) {
 	var a = this.a.local2World(this.anchr1);
 	var b = this.b.local2World(this.anchr2);
 	var midpoint = v.add(a, v.clamp(v.sub(b, a), this.min));
 
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = "grey";
-	drawLine(ctx, point2canvas, a, b);
+	drawLine(ctx, demo, a, b);
 	ctx.strokeStyle = "red";
-	drawLine(ctx, point2canvas, a, midpoint);
+	drawLine(ctx, demo, a, midpoint);
 };
 
-cp.PivotJoint.prototype.draw = function(ctx, scale, point2canvas) {
+cp.PivotJoint.prototype.draw = function(ctx, scale, demo) {
 	var a = this.a.local2World(this.anchr1);
 	var b = this.b.local2World(this.anchr2);
 	ctx.strokeStyle = "grey";
 	ctx.fillStyle = "grey";
-	drawCircle(ctx, scale, point2canvas, a, 2);
-	drawCircle(ctx, scale, point2canvas, b, 2);
+	drawCircle(ctx, scale, demo, a, 2);
+	drawCircle(ctx, scale, demo, b, 2);
 };
 
-cp.GrooveJoint.prototype.draw = function(ctx, scale, point2canvas) {
+cp.GrooveJoint.prototype.draw = function(ctx, scale, demo) {
 	var a = this.a.local2World(this.grv_a);
 	var b = this.a.local2World(this.grv_b);
 	var c = this.b.local2World(this.anchr2);
 	
 	ctx.strokeStyle = "grey";
-	drawLine(ctx, point2canvas, a, b);
-	drawCircle(ctx, scale, point2canvas, c, 3);
+	drawLine(ctx, demo, a, b);
+	drawCircle(ctx, scale, demo, c, 3);
 };
 
-cp.DampedSpring.prototype.draw = function(ctx, scale, point2canvas) {
+cp.DampedSpring.prototype.draw = function(ctx, scale, demo) {
 	var a = this.a.local2World(this.anchr1);
 	var b = this.b.local2World(this.anchr2);
 
 	ctx.strokeStyle = "grey";
-	drawSpring(ctx, scale, point2canvas, a, b);
+	drawSpring(ctx, scale, demo, a, b);
 };
 
 var randColor = function() {
